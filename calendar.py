be6b2f1e309723703733d28367d1c71f041e7995
nuxratx <161661618+nuxratx@users.noreply.github.com>
2024-05-25 10:10:45 -0400

    Adding a calendar

---
+++ calendar.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 24 2024
+
+@author: Nusrat
+"""
+
+import calendar
+
+#instantiating a text_calendar instance of calendar.TextCalendar
+text_calendar = calendar.TextCalendar()
+#calling .formatyear to text_calendar after instantiation 
+text_calendar.pryear(2024)
+
+
+
