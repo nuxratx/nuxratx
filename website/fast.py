e88f0eb94e7f81609311be997c632c37da8157bb
nuxratx <161661618+nuxratx@users.noreply.github.com>
2024-09-10 19:57:57 -0400

    added pydantic class

--- website/fast.py
+++ website/fast.py
@@ -1,29 +1,37 @@
-from fastapi import Form, File, UploadFile, Request, FastAPI
+from fastapi import Form, File, UploadFile, Request, FastAPI, Depends
 from fastapi.staticfiles import StaticFiles
 from typing import List
-from fastapi.responses import ORJSONResponse
+from fastapi.responses import HTMLResponse
+from pydantic import BaseModel
 from fastapi.templating import Jinja2Templates
 import subprocess
+
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
 #In python assigning a variable with type it can be defined with a colon like below 
+
+class Base(BaseModel):
+        contact: str | None
+        proxy1: str | None
+        proxy2: str | None
+        proxy3: str | None
+        proxy4: str | None
+        fname1: str | None
+        fname2: str | None
+        fname3: str | None
+        fname4: str | None
+        text5: str | None
+    
+
+
 @app.post("/submit")
-def submit(
-        contact:  str = Form(""),
-        proxy1: str = Form(""),
-        proxy2: str = Form(""),
-        proxy3: str = Form(""),
-        proxy4: str = Form(""),
-        fname1: str = Form(""),
-        fname2: str = Form(""),
-        fname3: str = Form(""),
-        fname4: str = Form(""),
-        text5: str = Form("")
-):
-   print("radio: " + contact, "\nCheckbox: " + proxy1, "\ncheckbox" + proxy1,
-         "\ncheckbox" + proxy2, "\ncheckbox" + proxy3, "\ncheckbox" + proxy4, 
-         "\ntext" + fname1, "\ntext" + fname2, "\ntext" + fname3, "\ntext" + fname4, "\ntext5 : " + text5)
+def submit(base: Base = Depends()):
+   return base
 
 app.mount("/", StaticFiles(directory="templates",html = True), name="templates")
+
+@app.get("/", response_class=HTMLResponse)
+def main(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
